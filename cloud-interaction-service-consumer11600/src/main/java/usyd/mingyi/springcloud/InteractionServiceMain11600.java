685f43bb4ff067d9b346ec0a481bd4acfb84684e
MingyiLi-USYD <lmy741917776@gmail.com>
2023-10-03 17:02:54 +1100

    remove unused yaml

--- cloud-interaction-service-consumer11600/src/main/java/usyd/mingyi/springcloud/InteractionServiceMain11600.java
+++ cloud-interaction-service-consumer11600/src/main/java/usyd/mingyi/springcloud/InteractionServiceMain11600.java
@@ -3,9 +3,7 @@ package usyd.mingyi.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "usyd.mingyi.common")
